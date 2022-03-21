887babfc348d6d6fee787b29864bbf990a4c4d73
Bi Wu <alerk_star@outlook.com>
2022-03-21 23:02:49 +0800

    integration test;

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 
 This example is based on a youtube tutorial - [Solidity, Blockchain, and Smart Contract Course – Beginner to Expert Python Tutorial](https://www.youtube.com/watch?v=M576WGiDBdQ&t=42543s)
 
-
--- scripts/deploy_and_upgrade.py
+++ scripts/deploy_and_upgrade.py
@@ -11,22 +11,32 @@ from scripts.helpful_scripts import get_account, encode_function_data, upgrade
 
 
 def main():
+    verify = config["networks"][network.show_active()].get("verify", False)
+    print(f"Will publish all contracts: {str(verify)}")
     account = get_account()
     print(f"Depoying to {network.show_active()}...")
 
-    box = Box.deploy({"from": account})
+    box = Box.deploy(
+        {"from": account},
+        publish_source=verify,
+    )
     print(box.retrieve())
 
-    proxy_admin = ProxyAdmin.deploy({"from": account})
+    proxy_admin = ProxyAdmin.deploy(
+        {"from": account},
+        publish_source=verify,
+    )
 
-    initializer = box.store, 1
-    box_encoded_initializer_function = encode_function_data(initializer)
+    # initializer = box.store, 1
+    # box_encoded_initializer_function = encode_function_data(initializer)
+    box_encoded_initializer_function = encode_function_data()
 
     proxy = TransparentUpgradeableProxy.deploy(
         box.address,
         proxy_admin.address,
         box_encoded_initializer_function,
-        {"from": account, "gast_limit": 1000000},
+        {"from": account, "gas_limit": 1000000},
+        # publish_source=verify,
     )
     print(f"Proxy deployed at {proxy}, you can upgrade to v2!")
     proxy_box = Contract.from_abi("Box", proxy.address, Box.abi)
@@ -35,17 +45,19 @@ def main():
     print(f"Box value is {proxy_box.retrieve()}")
 
     # upgrade
-    box_v2 = BoxV2.deploy({"from": account})
+    box_v2 = BoxV2.deploy(
+        {"from": account},
+        publish_source=verify,
+    )
     upgrade_tx = upgrade(
-      account,
-      proxy,
-      box_v2.address,
-      proxy_admin_contract=proxy_admin,
+        account,
+        proxy,
+        box_v2.address,
+        proxy_admin_contract=proxy_admin,
     )
     upgrade_tx.wait(1)
 
-    proxy_box = Contract.from_abi('BoxV2', proxy.address, BoxV2.abi)
+    proxy_box = Contract.from_abi("BoxV2", proxy.address, BoxV2.abi)
     increase_tx = proxy_box.increment({"from": account})
     increase_tx.wait(1)
     print(proxy_box.retrieve())
-
